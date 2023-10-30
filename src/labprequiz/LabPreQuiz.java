916257c556a41180d19fd27e22e55efccad028a5
2022271-KAS <2022271@student.cct.ie>
2023-10-30 20:20:18 +0000

    Codded. Orderable interface created. Enums created. Kitchen class codded. Main code codded.

--- src/labprequiz/LabPreQuiz.java
+++ src/labprequiz/LabPreQuiz.java
@@ -1,8 +1,35 @@
 package labprequiz;
+
+import ioutils.IOUtils;
+import labprequiz.enums.*;
+
 public class LabPreQuiz {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        IOUtils myInput = new IOUtils();
+        Kitchen kitchen = new Kitchen();
+        System.out.println("Welcome to our restaurant!");
+
+        try {
+            
+            System.out.println("Before I take your order, do you have any allergies?");
+            System.out.println("Here goes a list with the allergens in case you have missed it.");
+            kitchen.displayAllergens();
+            System.out.println();
+            System.out.println("Ready now? Cool, let's go!");
+
+            kitchen.order();
+            System.out.println("Thank you so much!");
+                        
+            
+
+        } catch (Exception e) {
+            System.out.println("Something went wrong!");
+        } finally {
+            System.out.println("I hope you have enjoyed your meal! See you again!");
+        }
+
     }
-    
+
 }
